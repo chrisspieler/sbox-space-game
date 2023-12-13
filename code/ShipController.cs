81955a21270e187deb5bab6f955c837d9f509aac
ducc <spielerc@gmail.com>
2023-12-13 04:57:14 -0700

    Visualize retrorocket force

--- code/ShipController.cs
+++ code/ShipController.cs
@@ -6,21 +6,18 @@ public sealed class ShipController : Component
 	[Property] public PhysicsComponent Rigidbody { get; set; }
 	[Property] public GameObject PartsContainer { get; set; }
 	[Property] public float Speed { get; set; } = 100f;
-	[Property] public float RetrorocketForce { get; set; } = 150f;
+	[Property] public float RetrorocketForceScale { get; set; } = 150f;
+	public Vector3 MainThrusterForce { get; private set; }
+	public Vector3 RetrorocketForce { get; private set; }
 
 	protected override void OnUpdate()
 	{
 		var inputDir = Input.AnalogMove;
+		MainThrusterForce = inputDir * Speed * Time.Delta;
 		var velocity = Rigidbody.Velocity;
-		velocity += inputDir * Speed * Time.Delta;
-		if ( inputDir.x == 0 || MathF.Sign( inputDir.x ) != MathF.Sign( velocity.x ) )
-		{
-			velocity.x = velocity.x.Approach( 0f, RetrorocketForce * Time.Delta );
-		}
-		if ( inputDir.y == 0 || MathF.Sign( inputDir.y ) != MathF.Sign( velocity.y ) )
-		{
-			velocity.y = velocity.y.Approach( 0f, RetrorocketForce * Time.Delta );
-		}
+		velocity += MainThrusterForce;
+		RetrorocketForce = GetRetrorocketForce( inputDir, velocity );
+		velocity += RetrorocketForce;
 		Rigidbody.Velocity = velocity;
 		if ( inputDir.LengthSquared < 0.01f )
 		{
@@ -29,5 +26,37 @@ public sealed class ShipController : Component
 		var fromRot = PartsContainer.Transform.Rotation;
 		var toRot = Rotation.LookAt( inputDir, Vector3.Up );
 		PartsContainer.Transform.Rotation = Rotation.Lerp( fromRot, toRot, 3f * Time.Delta );
+	}
+
+	private Vector3 GetRetrorocketForce( Vector3 inputDir, Vector3 velocity )
+	{
+		var force = Vector3.Zero;
+		if ( velocity.LengthSquared < 0.1f )
+			return -velocity * Time.Delta;
+
+		if ( inputDir.x == 0 || MathF.Sign( inputDir.x ) != MathF.Sign( velocity.x ) )
+		{
+			var xForce = RetrorocketForceScale * Time.Delta;
+			// Retrorocket force is applied in the opposite direction of velocity.
+			xForce *= -MathF.Sign( velocity.x );
+			// If the amount of xForce would flip the sign of x velocity...
+			if ( MathF.Abs( velocity.x ) < MathF.Abs( xForce ) )
+			{
+				// ...then just apply enough force to zero out x velocity.
+				xForce = MathF.Abs( velocity.x );
+			}
+			force += Vector3.Zero.WithX( xForce );
+		}
+		if ( inputDir.y == 0 || MathF.Sign( inputDir.y ) != MathF.Sign( velocity.y ) )
+		{
+			var yForce = RetrorocketForceScale * Time.Delta;
+			yForce *= -MathF.Sign( velocity.y );
+			if ( MathF.Abs( velocity.y ) < MathF.Abs( yForce ) )
+			{
+				yForce = MathF.Abs( velocity.y );
+			}
+			force += Vector3.Zero.WithY( yForce );
+		}
+		return force;
 	}
 }
---
+++ code/Thruster.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+
+public sealed class Thruster : Component
+{
+	[Property] public GameObject EffectPrefab { get; set; }
+	private GameObject _effectInstance { get; set; }
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+	}
+}
