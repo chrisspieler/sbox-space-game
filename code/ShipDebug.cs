44eca0289e955678c5076d59c966bdf107ba8908
ducc <spielerc@gmail.com>
2023-12-13 03:31:37 -0700

    Rate limit ShipDebug updates

--- code/ShipDebug.cs
+++ code/ShipDebug.cs
@@ -2,10 +2,22 @@ using Sandbox;
 
 public sealed class ShipDebug : Component
 {
+	[Property] public float UpdateInterval { get; set; } = 0.15f;
+	private TimeSince _lastUpdateTime = 0f;
+	private Vector3 _lastPosition;
+	private Vector3 _lastVelocity;
+
 	protected override void OnUpdate()
 	{
+		if ( _lastUpdateTime >= UpdateInterval )
+		{
+			_lastPosition = Transform.Position;
+			_lastVelocity = Components.Get<PhysicsComponent>()?.Velocity ?? Vector3.Zero;
+			_lastUpdateTime = 0f;
+		}
+
 		Gizmo.Draw.Color = Color.White;
-		var velocity = Components.Get<PhysicsComponent>()?.Velocity ?? Vector3.Zero;
-		Gizmo.Draw.ScreenText( $"Position: {Transform.Position}, Velocity: {velocity}", new Vector2( 25f, 25f ), "Consolas", 12, TextFlag.Left );
+		
+		Gizmo.Draw.ScreenText( $"Position: {_lastPosition}, Velocity: {_lastVelocity}", new Vector2( 25f, 25f ), "Consolas", 12, TextFlag.Left );
 	}
 }
