92de699069b46bc4bcda18af383d6b262cba5173
ducc <spielerc@gmail.com>
2024-02-17 15:14:58 -0700

    Add CursorPanel, crosshair.svg, Attribution GameResource

---
+++ code/Attribution.cs
@@ -0,0 +1,11 @@
+﻿namespace Sandbox;
+
+[GameResource("Attribution", "attrb", "Credit given to the original author of an asset.", Icon = "menu_book")]
+public class Attribution : GameResource
+{
+	[Property] public string Author { get; set; }
+	[Property] public string Name { get; set; }
+	[Property] public string Source { get; set; }
+	[Property] public string License { get; set; }
+	[Property] public string AssetPath { get; set; }
+}
