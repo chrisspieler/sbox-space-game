f963c7d82a4933bbd24b386d50a2c654dca5ea57
ducc <spielerc@gmail.com>
2024-02-17 21:24:10 -0700

    Add FloatingPanel, SelectionPanel. Select GameObjects on hover.

--- code/MouseSelector.cs
+++ code/MouseSelector.cs
@@ -9,6 +9,7 @@ public sealed class MouseSelector : Component
 	private GameObject _hovered;
 	[Property] public TagSet FilterWithAny { get; set; }
 	[Property] public TagSet FilterWithoutAny { get; set; }
+	[Property] public SelectionPanel HoveredIcon { get; set; }
 
 	protected override void OnUpdate()
 	{
@@ -16,6 +17,11 @@ public sealed class MouseSelector : Component
 		if ( hovered != _hovered )
 		{
 			_hovered = hovered;
+			if ( HoveredIcon is not null )
+			{
+				HoveredIcon.Target = _hovered;
+				HoveredIcon.Enabled = _hovered.IsValid();
+			}
 			OnHoveredChanged?.Invoke( _hovered );
 		}
 	}
