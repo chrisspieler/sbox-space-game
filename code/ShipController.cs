72aa73c3a33d36f2d2384076dd3ab9dcba95c52f
ducc <spielerc@gmail.com>
2024-02-20 14:26:50 -0700

    Add Stabilizer

--- code/ShipController.cs
+++ code/ShipController.cs
@@ -6,39 +6,40 @@ public sealed class ShipController : Component
 	[Property] public Rigidbody Rigidbody { get; set; }
 	[Property] public GameObject PartsContainer { get; set; }
 	[Property] public float Acceleration { get; set; } = 200f;
-	[Property] public float RetrorocketForceScale { get; set; } = 150f;
-	[Property] public Vector3 MainThrusterForce => _mainThrusterForce;
+	[Property] public float TurnSpeed { get; set; } = 1.5f;
+	[Property, Category("Debug")] public Vector3 MainThrusterForce => _mainThrusterForce;
 	private Vector3 _mainThrusterForce;
-	[Property] public Vector3 RetrorocketForce => _retrorocketForce;
+	[Property, Category("Debug")] public Vector3 RetrorocketForce => _retrorocketForce;
 	private Vector3 _retrorocketForce;
-	public Rotation TargetRotation { get; private set; }
-
+	[Property, Category("Debug")] public Vector3 LastInputDir => _lastInputDir;
+	private Vector3 _lastInputDir { get; set; } = Vector3.Forward;
 	[Property, Category( "Equipment" )]
 	public GrappleBeam Grapple { get; set; }
+	[Property, Category( "Equipment" )]
+	public Stabilizer Stabilizer { get; set; }
 
-	[Property] public Vector3 LastInputDir => _lastInputDir;
-	private Vector3 _lastInputDir { get; set; } = Vector3.Forward;
-	[Property] public float TurnSpeed { get; set; } = 1.5f;
+	public Rotation TargetRotation { get; private set; }
 
 	protected override void OnUpdate()
 	{
-		Transform.Rotation = Transform.Rotation.Angles().WithRoll( 0f );
-		Rigidbody.AngularVelocity = Vector3.Zero;
 		var inputDir = Input.AnalogMove;
 		if ( !inputDir.IsNearZeroLength )
 		{
 			_lastInputDir = GetLastKeyboardDirection();
 		}
-		var velocity = Rigidbody.Velocity;
-		_mainThrusterForce = PartsContainer.Transform.Rotation.Forward * GetMainThrusterForce( inputDir );
-		velocity += _mainThrusterForce;
-		_retrorocketForce = GetRetrorocketForce( inputDir, velocity );
-		velocity += _retrorocketForce;
-		Rigidbody.Velocity = velocity;
+		UpdateThrusters( inputDir );
 		var fromRot = PartsContainer.Transform.Rotation;
 		TargetRotation = GetTargetRotation();
 		var rotationSpeed = GetRotationSpeed();
 		PartsContainer.Transform.Rotation = Rotation.Lerp( fromRot, TargetRotation, rotationSpeed * Time.Delta );
+	}
+
+	private void UpdateThrusters( Vector3 inputDir )
+	{
+		_mainThrusterForce = PartsContainer.Transform.Rotation.Forward * GetMainThrusterForce( inputDir );
+		_retrorocketForce = Stabilizer?.GetStabilizerForce() ?? Vector3.Zero;
+		Rigidbody.Velocity += _mainThrusterForce;
+		Rigidbody.Velocity += _retrorocketForce;
 	}
 
 	[ConVar("input_diagonal_key_grace")] 
@@ -76,38 +77,6 @@ public sealed class ShipController : Component
 	{
 		var acceleration = Acceleration * (Input.Down( "thrust" ) ? 1f : 0f);
 		return acceleration * Time.Delta;
-	}
-
-	private Vector3 GetRetrorocketForce( Vector3 inputDir, Vector3 velocity )
-	{
-		var force = Vector3.Zero;
-		if ( velocity.LengthSquared < 0.1f )
-			return -velocity * Time.Delta;
-
-		if ( inputDir.x == 0 || MathF.Sign( inputDir.x ) != MathF.Sign( velocity.x ) )
-		{
-			var xForce = RetrorocketForceScale * Time.Delta;
-			// Retrorocket force is applied in the opposite direction of velocity.
-			xForce *= -MathF.Sign( velocity.x );
-			// If the amount of xForce would flip the sign of x velocity...
-			if ( MathF.Abs( velocity.x ) < MathF.Abs( xForce ) )
-			{
-				// ...then just apply enough force to zero out x velocity.
-				xForce = MathF.Abs( velocity.x );
-			}
-			force += Vector3.Zero.WithX( xForce );
-		}
-		if ( inputDir.y == 0 || MathF.Sign( inputDir.y ) != MathF.Sign( velocity.y ) )
-		{
-			var yForce = RetrorocketForceScale * Time.Delta;
-			yForce *= -MathF.Sign( velocity.y );
-			if ( MathF.Abs( velocity.y ) < MathF.Abs( yForce ) )
-			{
-				yForce = MathF.Abs( velocity.y );
-			}
-			force += Vector3.Zero.WithY( yForce );
-		}
-		return force;
 	}
 
 	public bool IsGrappling => Grapple.IsValid() && Grapple.IsSlack;
---
+++ code/Stabilizer.cs
@@ -0,0 +1,115 @@
+using Sandbox;
+
+public sealed class Stabilizer : Component
+{
+	[ConVar("stabilizer_debug")]
+	public static bool Debug { get; set; }
+	[Property] public Rigidbody Rigidbody { get; set; }
+	[Property] public Rigidbody VelocityMatchTarget { get; set; }
+	[Property] public float VelocityMatchedThreshold { get; set; } = 3f;
+	[Property] public float VelocityMatchMaxDistance { get; set; } = 1000f;
+	[Property] public MouseSelector Selector { get; set; }
+	[Property] public float StabilizerPower { get; set; }
+
+	private Rigidbody _hoveredRigidbody;
+
+	protected override void OnStart()
+	{
+		Selector.OnHoveredChanged += OnHoveredChanged;
+	}
+
+	private void OnHoveredChanged( GameObject hovered )
+	{
+		var rigidbody = hovered?.Components?.Get<Rigidbody>();
+		if ( rigidbody == null )
+		{
+			// We're hovering over empty space, or something that has no velocity of its own.
+			_hoveredRigidbody = null;
+			return;
+		}
+		// If we're already matching velocity with the target, don't even offer to match velocity.
+		if ( Rigidbody.Velocity.Distance( rigidbody.Velocity ) > VelocityMatchedThreshold )
+		{
+			_hoveredRigidbody = rigidbody;
+		}
+	}
+
+	protected override void OnUpdate()
+	{
+		if ( !VelocityMatchTarget.IsValid() && !_hoveredRigidbody.IsValid() )
+			return;
+
+		// It's not reasonable to try to match velocity with something that's off-screen.
+		// How would you even know it's there?
+		if ( IsTargetOutOfRange() )
+		{
+			VelocityMatchTarget = null;
+		}
+
+		// TODO: Replace this with proper glyphs in the UI.
+		using ( Gizmo.Scope( "Glyph Placeholder" ) )
+		{
+			Gizmo.Draw.Color = Color.Yellow;
+
+			var position = _hoveredRigidbody?.Transform?.Position 
+				?? VelocityMatchTarget?.Transform?.Position
+				?? Vector3.Zero;
+
+			var message = (_hoveredRigidbody.IsValid() && _hoveredRigidbody != VelocityMatchTarget)
+				? "Press R to Match Velocity"
+				: "Press R to Stop Matching Velocity";
+
+			Gizmo.Draw.ScreenText( message, Scene.Camera.PointToScreenPixels( position ), "Consolas" );
+		}
+		if ( Input.Pressed( "stabilizer" ) )
+		{
+			VelocityMatchTarget = _hoveredRigidbody;
+		}
+	}
+
+	private bool IsTargetOutOfRange()
+	{
+		return !VelocityMatchTarget.IsValid()
+			|| VelocityMatchTarget
+				.Transform
+				.Position
+				.Distance( Transform.Position ) > VelocityMatchMaxDistance;
+	}
+
+	public Vector3 GetStabilizerForce()
+	{
+		if ( !Enabled || !VelocityMatchTarget.IsValid() || !Rigidbody.IsValid() )
+			return default;
+
+		if ( Rigidbody.Velocity.Distance(VelocityMatchTarget.Velocity) < VelocityMatchedThreshold )
+		{
+			VelocityMatchTarget = null;
+			return Vector3.Zero;
+		}
+
+		DrawDebug( Rigidbody.Velocity );
+		// Move our velocity towards the target's velocity.
+		var delta = VelocityMatchTarget.Velocity - Rigidbody.Velocity;
+		delta = delta.Clamp( -StabilizerPower, StabilizerPower );
+		return delta * Time.Delta;
+	}
+
+	private void DrawDebug( Vector3 currentVelocity )
+	{
+		if ( !Debug )
+			return;
+
+		if ( !VelocityMatchTarget.IsValid() )
+			return;
+
+		using ( Gizmo.Scope("Stabilizer Debug") )
+		{
+			var midpoint = Transform.Position.LerpTo( VelocityMatchTarget.Transform.Position, 0.5f );
+			Gizmo.Draw.Color = Color.Red;
+			Gizmo.Draw.Arrow( midpoint, midpoint + VelocityMatchTarget.Velocity );
+			Gizmo.Draw.Color = Color.Blue;
+			Gizmo.Draw.Arrow( midpoint, midpoint + currentVelocity );
+		}
+
+	}
+}
