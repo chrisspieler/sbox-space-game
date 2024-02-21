4f5f3c1c728961a621442ffe2ae137d751f91cf6
ducc <spielerc@gmail.com>
2024-02-20 18:40:21 -0700

    Add DeselectTime

--- code/MouseSelector.cs
+++ code/MouseSelector.cs
@@ -9,21 +9,32 @@ public sealed class MouseSelector : Component
 	private GameObject _hovered;
 	[Property] public TagSet FilterWithAny { get; set; }
 	[Property] public TagSet FilterWithoutAny { get; set; }
+	[Property] public float DeselectTime { get; set; } = 1f;
 	[Property] public SelectionPanel HoveredIcon { get; set; }
+
+	private TimeUntil _untilDeselect;
 
 	protected override void OnUpdate()
 	{
 		var hovered = GetHovered();
-		if ( hovered != _hovered )
+		if ( hovered == _hovered )
 		{
-			_hovered = hovered;
-			if ( HoveredIcon is not null )
-			{
-				HoveredIcon.Target = _hovered;
-				HoveredIcon.Enabled = _hovered.IsValid();
-			}
-			OnHoveredChanged?.Invoke( _hovered );
+			_untilDeselect = DeselectTime;
+			return;
 		}
+
+		// If something is hovered, don't immediately clear Hovered when there's nothing else to replace it.
+		// Having this grace period makes it easier to hover over small objects in empty space.
+		if ( _hovered.IsValid() && !hovered.IsValid() && !_untilDeselect )
+			return;
+
+		_hovered = hovered;
+		if ( HoveredIcon is not null )
+		{
+			HoveredIcon.Target = _hovered;
+			HoveredIcon.Enabled = _hovered.IsValid();
+		}
+		OnHoveredChanged?.Invoke( _hovered );
 	}
 
 	private GameObject GetHovered()
