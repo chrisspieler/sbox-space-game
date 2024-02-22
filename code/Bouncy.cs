940af8f79b0d526345cb39a1c96071b84dbd80b8
ducc <spielerc@gmail.com>
2024-02-21 21:20:12 -0700

    Add shield to base to bounce away asteroids

--- code/Bouncy.cs
+++ code/Bouncy.cs
@@ -9,6 +9,8 @@ public sealed class Bouncy : Component, Component.ICollisionListener
 
 	[Property] public float BounceFactor { get; set; } = 1f;
 	[Property] public TagSet BouncedTags { get; set; }
+	[Property] public TagSet IgnoredTags { get; set; }
+	[Property] public float AddedVelocity { get; set; } = 0f;
 	[Property] public float RehitDelay { get; set; } = 0.5f;
 
 	private readonly Dictionary<GameObject, TimeUntil> _recentlyHit = new();
@@ -34,6 +36,7 @@ public sealed class Bouncy : Component, Component.ICollisionListener
 
 		_recentlyHit[other.Other.GameObject] = RehitDelay;
 		other.Pongify( BounceFactor );
+		other.Other.Body.Velocity += Vector3.One * AddedVelocity;
 		OnBounce?.Invoke( other );
 	}
 	public void OnCollisionStop( CollisionStop other ) { }
@@ -44,8 +47,7 @@ public sealed class Bouncy : Component, Component.ICollisionListener
 		if ( _recentlyHit.ContainsKey( other ) )
 			return false;
 
-		return BouncedTags is null
-			|| BouncedTags.IsEmpty
-			|| other.Tags.HasAny( BouncedTags );
+		return (BouncedTags is null || BouncedTags.IsEmpty || other.Tags.HasAny( BouncedTags ))
+			&& (IgnoredTags is null || IgnoredTags.IsEmpty || !other.Tags.HasAny( IgnoredTags ));
 	}
 }
