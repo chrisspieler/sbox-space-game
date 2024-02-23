cdf9ac7200bc84b777b6299d2d5c3619da146073
ducc <spielerc@gmail.com>
2024-02-23 02:34:50 -0700

    Move cs files in to folders

--- code/Extensions/GameObjectExtensions.cs
+++ code/Extensions/GameObjectExtensions.cs
@@ -4,6 +4,24 @@ using Sandbox;
 
 public static class GameObjectExtensions
 {
+	public static Vector3 GetAbsolutePosition( this GameObject gameObject )
+	{
+		if ( !gameObject.IsValid() )
+			return Vector3.Zero;
+
+		var originSystem = gameObject.Scene.GetSystem<FloatingOriginSystem>();
+		return gameObject.Transform.Position + originSystem.TotalOriginShift;
+	}
+
+	public static void SetAbsolutePosition( this GameObject gameObject, Vector3 position )
+	{
+		if ( !gameObject.IsValid() )
+			return;
+
+		var originSystem = gameObject.Scene.GetSystem<FloatingOriginSystem>();
+		gameObject.Transform.Position = position - originSystem.TotalOriginShift;
+	}
+
 	public static void ColorFlash( this GameObject obj, TintEffect effect )
 	{
 		HashSet<GameObject> reached = new();
