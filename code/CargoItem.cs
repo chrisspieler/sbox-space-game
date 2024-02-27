3ad7d72ce45ee87bea493f321a89b0e888e0fde8
ducc <spielerc@gmail.com>
2024-02-26 21:38:38 -0700

    Add Pickup, CargoItem, iron ore

---
+++ code/CargoItem.cs
@@ -0,0 +1,11 @@
+﻿using Sandbox;
+
+[GameResource( "Cargo Item", "cargo", "An item which may be contained within a cargo hold or sold for credits.", Icon = "inventory_2" )]
+public class CargoItem : GameResource
+{
+	public string Name { get; set; }
+	[TextArea] 
+	public string Description { get; set; }
+	public GameObject PickupPrefab { get; set; }
+	public Texture InventoryIcon { get; set; }
+}
---
+++ code/Pickup.cs
@@ -0,0 +1,62 @@
+using Sandbox;
+using System;
+using System.Threading.Tasks;
+
+public sealed class Pickup : Component, Component.ITriggerListener
+{
+	[Property] public CargoItem Item { get; set; }
+	[Property] public GameObject PickupEffectPrefab { get; set; }
+	[Property, Category( "Animation" )]
+	public float AnimationDuration { get; set; } = 0.4f;
+	[Property, Category( "Animation" )] 
+	public Curve ScaleCurve { get; set; }
+	[Property, Category( "Animation" )] 
+	public Curve ZPositionCurve { get; set; }
+
+	private bool _triggered;
+
+	protected override void OnStart()
+	{
+		if ( Item is not null && Item.PickupPrefab is not null )
+		{
+			var prefabGo = Item.PickupPrefab.Clone();
+			prefabGo.Parent = GameObject;
+			prefabGo.Transform.Rotation = Rotation.FromYaw( Random.Shared.Float( 0f, 360f ) );
+			prefabGo.BreakFromPrefab();
+		}
+		GameObject.BreakFromPrefab();
+	}
+
+	public void OnTriggerEnter( Collider other ) 
+	{
+		if ( _triggered || !other.IsPlayer() )
+			return;
+
+		_triggered = true;
+
+		if ( PickupEffectPrefab is not null )
+		{
+			var effectGo = PickupEffectPrefab.Clone();
+			effectGo.Transform.Position = Transform.Position;
+		}
+		_ = DoAnimation();
+		// TODO: Add pickup to player cargo.
+	}
+
+	public void OnTriggerExit( Collider other ) { }
+
+	private async Task DoAnimation()
+	{
+		TimeUntil _untilAnimationFinished = AnimationDuration;
+		var startHeight = Transform.Position.z;
+		while ( !_untilAnimationFinished )
+		{
+			var t = _untilAnimationFinished.Fraction;
+			Transform.Scale = ScaleCurve.Evaluate( t );
+			var currentHeight = startHeight + ZPositionCurve.Evaluate( t );
+			Transform.Position = Transform.Position.WithZ( currentHeight );
+			await Task.Frame();
+		}
+		GameObject.Destroy();
+	}
+}
