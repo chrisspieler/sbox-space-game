b9622721a02c1ae330b454343ab08156a8256f9a
ducc <spielerc@gmail.com>
2024-02-29 05:10:03 -0700

    Fix beacon text appearing at completely the wrong position at long distances

--- code/Extensions/CameraComponentExtensions.cs
+++ code/Extensions/CameraComponentExtensions.cs
@@ -4,12 +4,14 @@ public static class CameraComponentExtensions
 {
 	public static Vector3 MouseToWorld( this CameraComponent camera )
 	{
-		if ( GameManager.ActiveScene?.Camera is null )
-			return Vector3.Zero;
-
 		var mouseNormalPos = Mouse.Position / Screen.Size;
-		var ray = GameManager.ActiveScene.Camera.ScreenNormalToRay( mouseNormalPos );
+		return camera.ScreenNormalToWorld( mouseNormalPos );
+	}
+
+	public static Vector3 ScreenNormalToWorld( this CameraComponent camera, Vector2 normal )
+	{
+		var centerRay = camera.ScreenNormalToRay( normal );
 		var worldPlane = new Plane( Vector3.Zero, Vector3.Up );
-		return worldPlane.Trace( ray ) ?? Vector3.Zero;
+		return worldPlane.Trace( centerRay ) ?? Vector3.Zero;
 	}
 }
