3f2e5364ccc3f50cc45cd323d76e31832c5576d6
ducc <spielerc@gmail.com>
2024-03-10 15:08:00 -0600

    Add a couple more upgrade thumbnails

---
+++ code/Utility/TranslucentFlag.cs
@@ -0,0 +1,30 @@
+﻿using Sandbox;
+
+public sealed class TranslucentFlag : Component, Component.ExecuteInEditor
+{
+
+	[Property] public ModelRenderer Renderer 
+	{
+		get => _renderer;
+		set
+		{
+			_renderer = value;
+			MakeTranslucent();
+		}
+	}
+	private ModelRenderer _renderer;
+
+	protected override void OnEnabled()
+	{
+		MakeTranslucent();
+	}
+
+	public void MakeTranslucent()
+	{
+		if ( Renderer?.SceneObject is null )
+		{
+			return;
+		}
+		Renderer.SceneObject.Flags.IsTranslucent = true;
+	}
+}
