9404fd925565e29370f22d1751cb07e3e226b25d
ducc <spielerc@gmail.com>
2024-03-11 08:46:27 -0600

    Clean up and bugfix stabilizer, double stabilizer velocity match distance

--- code/Player/Equipment/Stabilizer.cs
+++ code/Player/Equipment/Stabilizer.cs
@@ -7,7 +7,7 @@ public sealed class Stabilizer : Component
 	[Property] public Rigidbody Rigidbody { get; set; }
 	[Property] public Rigidbody VelocityMatchTarget { get; set; }
 	[Property] public float VelocityMatchedThreshold { get; set; } = 3f;
-	[Property] public float VelocityMatchMaxDistance { get; set; } = 1000f;
+	[Property] public float VelocityMatchMaxDistance { get; set; } = 2000f;
 	[Property] public float StabilizerPower { get; set; }
 	[Property] public SoundEvent LoopSound { get; set; }
 
@@ -28,51 +28,28 @@ public sealed class Stabilizer : Component
 			_hoveredRigidbody = null;
 			return;
 		}
-		// If we're already matching velocity with the target, don't even offer to match velocity.
+		// If the ship is moving at a velocity different from the hovered object...
 		if ( Rigidbody.Velocity.Distance( rigidbody.Velocity ) > VelocityMatchedThreshold )
 		{
+			// ...recognize the object as something we could potentially match velocity with.
 			_hoveredRigidbody = rigidbody;
 		}
 	}
 
 	protected override void OnUpdate()
 	{
-		ScreenManager.SetHoveredSelection( _hoveredRigidbody?.GameObject ?? VelocityMatchTarget?.GameObject );
-
-		if ( !VelocityMatchTarget.IsValid() && !_hoveredRigidbody.IsValid() )
-		{
-			_loopSoundHandle?.Stop( 0.1f );
-			return;
-		}
+		UpdateSelectionUI();
 
 		// It's not reasonable to try to match velocity with something that's off-screen.
 		// How would you even know it's there?
-		if ( IsTargetOutOfRange() )
+		if ( !CanMatchTarget( VelocityMatchTarget ) )
 		{
 			VelocityMatchTarget = null;
 		}
 
-		if ( _loopSoundHandle?.IsPlaying != true )
-		{
-			_loopSoundHandle = Sound.Play( LoopSound, Transform.Position );
-		}
-		_loopSoundHandle.Position = Transform.Position;
+		UpdateSound();
+		UpdateMatchText();
 
-		// TODO: Replace this with proper glyphs in the UI.
-		using ( Gizmo.Scope( "Glyph Placeholder" ) )
-		{
-			Gizmo.Draw.Color = Color.Yellow;
-
-			var position = _hoveredRigidbody?.Transform?.Position 
-				?? VelocityMatchTarget?.Transform?.Position
-				?? Vector3.Zero;
-
-			var message = (_hoveredRigidbody.IsValid() && _hoveredRigidbody != VelocityMatchTarget)
-				? "Press R to Match Velocity"
-				: "Press R to Stop Matching Velocity";
-
-			Gizmo.Draw.ScreenText( message, Scene.Camera.PointToScreenPixels( position ), "Consolas" );
-		}
 		if ( Input.Pressed( "stabilizer" ) )
 		{
 			VelocityMatchTarget = _hoveredRigidbody;
@@ -84,13 +61,57 @@ public sealed class Stabilizer : Component
 		_loopSoundHandle?.Stop( 0.1f );
 	}
 
-	private bool IsTargetOutOfRange()
+	private bool CanMatchTarget( Rigidbody rb )
 	{
-		return !VelocityMatchTarget.IsValid()
-			|| VelocityMatchTarget
-				.Transform
-				.Position
-				.Distance( Transform.Position ) > VelocityMatchMaxDistance;
+		return rb.IsValid()
+			&& rb.Transform.Position
+				.Distance( Transform.Position ) < VelocityMatchMaxDistance;
+	}
+
+	private void UpdateSelectionUI()
+	{
+		var selection = CanMatchTarget( _hoveredRigidbody )
+			? _hoveredRigidbody.GameObject
+			: VelocityMatchTarget?.GameObject;
+		ScreenManager.SetHoveredSelection( selection );
+	}
+
+	private void UpdateSound()
+	{
+		if ( !VelocityMatchTarget.IsValid() )
+		{
+			_loopSoundHandle?.Stop( 0.1f );
+			return;
+		}
+		if ( _loopSoundHandle?.IsPlaying != true )
+		{
+			_loopSoundHandle = Sound.Play( LoopSound, Transform.Position );
+		}
+		_loopSoundHandle.Position = Transform.Position;
+	}
+
+	private void UpdateMatchText()
+	{
+		var canMatchHovered = CanMatchTarget( _hoveredRigidbody );
+		// There's no reason to draw any text if we can't cancel or start matching velocity.
+		if ( !VelocityMatchTarget.IsValid() && !canMatchHovered )
+			return;
+
+		// TODO: Replace this with proper glyphs in the UI.
+		using ( Gizmo.Scope( "Glyph Placeholder" ) )
+		{
+			Gizmo.Draw.Color = Color.Yellow;
+
+			var position = _hoveredRigidbody?.Transform?.Position
+				?? VelocityMatchTarget?.Transform?.Position
+				?? Vector3.Zero;
+
+			var message = (CanMatchTarget( _hoveredRigidbody ) && _hoveredRigidbody != VelocityMatchTarget)
+				? "Press R to Match Velocity"
+				: "Press R to Stop Matching Velocity";
+
+			Gizmo.Draw.ScreenText( message, Scene.Camera.PointToScreenPixels( position ), "Consolas" );
+		}
 	}
 
 	public Vector3 GetStabilizerForce()
