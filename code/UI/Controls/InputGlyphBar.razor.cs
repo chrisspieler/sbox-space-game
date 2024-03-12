683ff66321e4807c50bffcc1bd6c43bd77a528c3
ducc <spielerc@gmail.com>
2024-03-12 11:40:02 -0600

    Copy InputGlyphBar/Control from another project

---
+++ code/UI/Controls/InputGlyphBar.razor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sandbox.UI;
+
+[StyleSheet()]
+public partial class InputGlyphBar : Panel
+{
+	public static InputGlyphBar Instance { get; private set; }
+	private Dictionary<string, InputGlyphData> Glyphs { get; set; } = new();
+
+	public InputGlyphBar()
+	{
+		Instance = this;
+	}
+
+	public void AddGlyph( InputGlyphData data )
+	{
+		Glyphs[data.ActionName] = data;
+		DeleteChildren( true );
+		StateHasChanged();
+	}
+
+	public override void Tick()
+	{
+		var glyphs = Glyphs.Values.ToList();
+		foreach ( var glyph in glyphs )
+		{
+			if ( glyph.RemovalPredicate?.Invoke() == true )
+			{
+				Glyphs.Remove( glyph.ActionName );
+			}
+		}
+		StateHasChanged();
+	}
+
+	private void DrawGlyph( InputGlyphData data )
+	{
+		var glyphControl = new InputGlyphControl();
+		glyphControl.GlyphData = data;
+		AddChild( glyphControl );
+	}
+}
---
+++ code/UI/InputGlyphData.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public struct InputGlyphData
+{
+	public string ActionName { get; set; }
+	public string DisplayText { get; set; }
+	public Func<bool> RemovalPredicate { get; set; }
+}
