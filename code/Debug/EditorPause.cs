83ab89fc13b5900a5a295fc470aec8fe693c2d92
ducc <spielerc@gmail.com>
2024-03-14 00:57:51 -0600

    Make QT drive available for purchase, add thumbnail, balance tweaks

---
+++ code/Debug/EditorPause.cs
@@ -0,0 +1,17 @@
+﻿using Sandbox;
+
+public class EditorPause : GameObjectSystem
+{
+	public EditorPause( Scene scene ) : base( scene )
+	{
+		Listen( Stage.UpdateBones, 0, OnTick, "Check Pause" );
+	}
+
+	private void OnTick()
+	{
+		if ( Game.IsEditor && Input.Pressed( "pause" ) )
+		{
+			Game.IsPaused = !Game.IsPaused;
+		}
+	}
+}
