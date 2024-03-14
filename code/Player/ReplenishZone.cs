fcc810b67178e310fa227139c73c6ca3c5587546
ducc <spielerc@gmail.com>
2024-03-14 10:36:06 -0600

    Automatically replenish fuel/health when arriving at base

---
+++ code/Player/ReplenishZone.cs
@@ -0,0 +1,28 @@
+﻿using Sandbox;
+
+public sealed class ReplenishZone : Component, Component.ITriggerListener
+{
+	[Property] public bool Refuel { get; set; } = true;
+	[Property] public bool Repair { get; set; } = true;
+
+	public void OnTriggerEnter( Collider other )
+	{
+		if ( !other.IsPlayer() )
+			return;
+
+		var ship = ShipController.GetCurrent();
+		if ( ship is null )
+			return;
+
+		if ( Refuel && ship.Fuel?.Enabled == true && ship.Fuel.CurrentAmount < ship.Fuel.MaxCapacity )
+		{
+			ship.Fuel.CurrentAmount = ship.Fuel.MaxCapacity;
+			ScreenManager.ShowTextPanel( "FUEL REPLENISHED", ship.Transform.Position, false );
+		}
+		if ( Repair && ship.Hull?.Enabled == true && ship.Hull.CurrentHealth < ship.Hull.MaxHealth )
+		{
+			ship.Hull.CurrentHealth = ship.Hull.MaxHealth;
+			ScreenManager.ShowTextPanel( "HULL REPAIRED", ship.Transform.Position + Vector3.Backward * 100f, false );
+		}
+	}
+}
