1439057a330cde740469e04558f6d6b08e6474d3
ducc <spielerc@gmail.com>
2024-03-15 17:06:25 -0600

    Add CollisionBubbler, fix asteroids not taking damage from collisions

---
+++ code/Utility/CollisionBubbler.cs
@@ -0,0 +1,49 @@
+﻿using Sandbox;
+
+public sealed class CollisionBubbler : Component, Component.ICollisionListener
+{
+	public void OnCollisionStart( Collision other ) 
+	{
+		var parent = GameObject.Parent;
+		while ( parent is not null )
+		{
+			var listener = parent.Components.Get<ICollisionListener>();
+			if ( listener is not null )
+			{
+				listener.OnCollisionStart( other );
+				break;
+			}
+			parent = parent.Parent;
+		}
+	}
+
+	public void OnCollisionStop( CollisionStop other ) 
+	{
+		var parent = GameObject.Parent;
+		while ( parent is not null )
+		{
+			var listener = parent.Components.Get<ICollisionListener>();
+			if ( listener is not null )
+			{
+				listener.OnCollisionStop( other );
+				break;
+			}
+			parent = parent.Parent;
+		}
+	}
+
+	public void OnCollisionUpdate( Collision other ) 
+	{
+		var parent = GameObject.Parent;
+		while ( parent is not null )
+		{
+			var listener = parent.Components.Get<ICollisionListener>();
+			if ( listener is not null )
+			{
+				listener.OnCollisionUpdate( other );
+				break;
+			}
+			parent = parent.Parent;
+		}
+	}
+}
