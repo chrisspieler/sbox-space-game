630d1cd8a0b9ef8e111d07a71f3b9cc7fac8641d
ducc <spielerc@gmail.com>
2024-03-19 23:35:22 -0600

    Add drone_spawn_debug and drone_spawn

--- code/AI/DroneSpawner.cs
+++ code/AI/DroneSpawner.cs
@@ -5,12 +5,16 @@ using System.Linq;
 
 public sealed class DroneSpawner : Component
 {
+	[ConVar( "drone_spawn_debug" )]
+	public static bool Debug { get; set; } = false;
+
 	[Property] public GameObject DronePrefab { get; set; }
 	[Property] public int DroneCount { get; set; } = 15;
 	[Property] public bool SpawnOnStart { get; set; } = true;
 
 	protected override void OnStart()
 	{
+		DronePrefab ??= ResourceLibrary.Get<PrefabFile>( "prefabs/drones/mining_drone.prefab" ).GetPrefabScene();
 		if ( SpawnOnStart )
 		{
 			ForceSpawnMany();
@@ -22,7 +26,10 @@ public sealed class DroneSpawner : Component
 		var chunk = GameObject.GetWorldChunk();
 		var bounds = chunk.GetChunkWorldBoundsAbsolute();
 		var position = bounds.RandomPointInside;
-		Log.Info( $"Spawning drone in chunk {chunk} at position: {position}" );
+		if ( Debug )
+		{
+			Log.Info( $"Spawning drone in chunk {chunk} at position: {position}" );
+		}
 		return DronePrefab.Clone( position );
 	}
 
@@ -38,5 +45,17 @@ public sealed class DroneSpawner : Component
 		{
 			yield return SpawnOne();
 		}
+	}
+
+	[ConCmd("drone_spawn")]
+	public static void SpawnCommand()
+	{
+		if ( Game.ActiveScene is null || Game.ActiveScene.Camera is null )
+			return;
+
+		var worldPos = Game.ActiveScene.Camera.ScreenNormalToWorld( 0.5f );
+		var spawnerGo = new GameObject( true, "Drone Spawner" );
+		spawnerGo.Transform.Position = worldPos;
+		spawnerGo.Components.Create<DroneSpawner>();
 	}
 }
