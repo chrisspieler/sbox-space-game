a7d51fa315dfaebcd50a2fb9ef14e068fed32901
ducc <spielerc@gmail.com>
2024-03-22 16:30:31 -0600

    Update distant asteroid transform only four times per second.

--- code/World/DistantAsteroidEffect.cs
+++ code/World/DistantAsteroidEffect.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 
 public sealed class DistantAsteroidEffect : Component, IOriginShiftListener
 {
+	[ConVar( "distant_asteroid_update_interval" )]
+	public static float UpdateIntervalSeconds { get; set; } = 0.25f;
+
 	[Property] public Model AsteroidModel { get; set; }
 	[Property] public Material AsteroidMaterial { get; set; }
 	[Property] public BBox AsteroidSpawnBounds { get; set; } = BBox.FromPositionAndSize( Vector3.Zero, ( Vector3.One * 5000f ).WithZ( 0f ) );
@@ -12,18 +15,24 @@ public sealed class DistantAsteroidEffect : Component, IOriginShiftListener
 
 	private List<SceneModel> _asteroids = new();
 
+	private RealTimeSince _sinceLastUpdate;
+
 	protected override void OnUpdate()
 	{
+		if ( _sinceLastUpdate < UpdateIntervalSeconds )
+			return;
+
 		for ( int i = 0; i < _asteroids.Count; i++ )
 		{
 			var asteroid = _asteroids[i];
 			var scaleSpeed = 1f - asteroid.Transform.Scale.x / 20f;
-			asteroid.Rotation *= Rotation.FromPitch( 10f * Time.Delta * scaleSpeed );
-			asteroid.Position += Vector3.Right * 20f * Time.Delta;
-			asteroid.Update( Time.Delta );
+			asteroid.Rotation *= Rotation.FromPitch( 10f * _sinceLastUpdate * scaleSpeed );
+			asteroid.Position += Vector3.Right * 20f * _sinceLastUpdate;
+			asteroid.Update( _sinceLastUpdate );
 			var targetTint = asteroid.ColorTint.WithAlpha( 1f );
 			asteroid.ColorTint = Color.Lerp( asteroid.ColorTint, targetTint, Time.Delta / 2f );
 		}
+		_sinceLastUpdate = 0f;
 	}
 
 	public void OnAfterOriginShift( Vector3 offset ) 
@@ -37,6 +46,7 @@ public sealed class DistantAsteroidEffect : Component, IOriginShiftListener
 	protected override void OnEnabled()
 	{
 		CreateAsteroids();
+		_sinceLastUpdate = 0f;
 	}
 
 	protected override void OnDisabled()
