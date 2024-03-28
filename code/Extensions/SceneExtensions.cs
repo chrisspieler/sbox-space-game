fbf6e907b19717580c2ffc6f7d4e70fdc4859dc7
ducc <spielerc@gmail.com>
2024-03-27 20:41:04 -0600

    Don't allow data wipe except on main menu

---
+++ code/Extensions/SceneExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+
+public static class SceneExtensions
+{
+	public static bool IsMainMenu( this Scene scene )
+	{
+		if ( !scene.IsValid() )
+			return false;
+
+		return scene.Title == "main_menu";
+	}
+}
