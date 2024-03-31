632d1d165acdfd0e07f71d0d381692404f39f564
ducc <spielerc@gmail.com>
2024-03-30 22:41:55 -0600

    Main menu camera now pans across world

--- code/Utility/Mover.cs
+++ code/Utility/Mover.cs
@@ -4,9 +4,20 @@ public sealed class Mover : Component
 {
 	[Property] public Vector3 Direction { get; set; }
 	[Property] public float UnitsPerSecond { get; set; }
+	[Property] public float? LerpSpeed { get; set; } = 5f;
+
+	private Vector3 _currentTranslation;
 
 	protected override void OnUpdate()
 	{
-		Transform.Position += Direction.Normal * UnitsPerSecond * Time.Delta;
+		if ( LerpSpeed.HasValue )
+		{
+			_currentTranslation = _currentTranslation.LerpTo( UnitsPerSecond, Time.Delta * LerpSpeed.Value );
+		}
+		else
+		{
+			_currentTranslation = UnitsPerSecond;
+		}
+		Transform.Position += Direction.Normal * _currentTranslation * Time.Delta;
 	}
 }
