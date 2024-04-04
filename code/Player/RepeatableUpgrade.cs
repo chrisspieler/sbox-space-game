88911403cf8b65f1c76d5091b80494534474c1f7
ducc <spielerc@gmail.com>
2024-04-04 03:54:45 -0600

    Stat work on repeatable upgrade

---
+++ code/Player/RepeatableUpgrade.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+
+[GameResource( "Repeatable Ship Upgrade", "repup", "An upgrade that may be repeatedly applied to the ship.", Icon = "upgrade" )]
+public class RepeatableUpgrade : GameResource
+{
+	/// <summary>
+	/// Given the current level, returns the number of credits that would
+	/// be required to upgrade to the next level.
+	/// </summary>
+	public delegate int LevelCostCalculation( int currentLevel );
+	/// <summary>
+	/// Invoked after increasing the level of an upgrade. Given the new level,
+	/// applies the effect of the upgrade in-game.
+	/// </summary>
+	public delegate void LevelIncreaseAction( int newLevel );
+
+	public string Name { get; set; }
+	public string FunctionText { get; set; }
+	public Texture Thumbnail { get; set; }
+	public List<Upgrade> EquipmentPrereqs { get; set; }
+	public int MaxLevel { get; set; } = 15;
+	public LevelCostCalculation GetCost { get; set; }
+	public LevelIncreaseAction OnIncreaseLevel { get; set; }
+}
