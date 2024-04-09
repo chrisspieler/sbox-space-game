10b03dd959a6c4d7c2937de587a0ecf5a03ac305
ducc <spielerc@gmail.com>
2024-04-09 09:14:10 -0600

    Import sbox-radial-menu pretty much as-is

---
+++ code/UI/Controls/RadialMenuItem.razor.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+public partial class RadialMenuItem : Panel
+{
+	public bool IsSelected { get; set; }
+	public RadialMenu.Item Item { get; set; }
+
+	public RadialMenuItem() { }
+
+	public override void OnLayout( ref Rect layoutRect )
+	{
+		base.OnLayout( ref layoutRect );
+
+		var halfWidth = layoutRect.Width / 2f;
+		var halfHeight = layoutRect.Height / 2f;
+
+		layoutRect.Left -= halfWidth;
+		layoutRect.Top -= halfHeight;
+		layoutRect.Right -= halfWidth;
+		layoutRect.Bottom -= halfHeight;
+	}
+
+	protected override void OnParametersSet()
+	{
+		BindClass( "selected", () => IsSelected );
+		base.OnParametersSet();
+	}
+
+	protected override int BuildHash()
+	{
+		return Item.GetHashCode();
+	}
+}
---
+++ code/UI/QuickMenuPanel.razor.cs
@@ -0,0 +1,11 @@
+﻿using Sandbox;
+
+public partial class QuickMenuPanel : PanelComponent
+{
+	public RadialMenu Menu { get; set; }
+
+	protected override void OnUpdate()
+	{
+		Menu?.BuildInput();
+	}
+}
