235409f38f9e5f5c05a8a43a7843dd5e1a0d5863
ducc <spielerc@gmail.com>
2024-05-22 18:52:48 -0600

    Add OriginShiftAction component, disable/reenable shop GameObject on world origin shift to guard against https://github.com/Facepunch/sbox-issues/issues/5555

---
+++ code/World/OriginShiftAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sandbox;
+
+[Group( "World")]
+public class OriginShiftAction : Component, IOriginShiftListener
+{
+	[Property] public Action<Vector3> OnBeforeOriginShifted { get; set; }
+	[Property] public Action<Vector3> OnAfterOriginShifted { get; set; }
+
+	public void OnBeforeOriginShift( Vector3 offset ) => OnBeforeOriginShifted?.Invoke( offset );
+	public void OnAfterOriginShift( Vector3 offset ) => OnAfterOriginShifted?.Invoke( offset );
+}
